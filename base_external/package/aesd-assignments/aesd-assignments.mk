ac49694e309e4e5ebf9516834063a4559a0205e5
Oleksii Khovan <ahovan@gmail.com>
2025-01-13 22:00:16 +0100

    fix commit hash for assigment-3 repo

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 4eb821860447f8427d7fc43700c77e286e53c18c
+AESD_ASSIGNMENTS_VERSION = 9ba398297ef31b98ad45f2cc4a5d0bb37a344639
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
